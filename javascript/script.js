5d39a7b1b045a731de4a39bfe37522f4bc6d299c
Jhon Ramirez <jhramirez9716@gmail.com>
2025-04-22 20:20:28 -0500

    update

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+/*Comentario Multi-linea*/
+// Comentario de una linea
+
+console.log("Hola, Jhon!");
+
+
+
+console.log("1");
+//console.log("2");
+console.log("3");
+
+
+/*VARIABLES*/
+
+// Automática
+nombreVariable1 = "Jhon Ramirez"
+console.log(nombreVariable1)
+
+ /*OPERADORES*/
+ // ASIGNACION
+ 
+ 
+ /*TIPOS DE DATOS PRMITIVOS*/
+ // String, cadena de texto
