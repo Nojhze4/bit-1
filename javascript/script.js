426f00a6ed49b658f17f7a6cc124376e10055a96
Jhon Ramirez <jhramirez9716@gmail.com>
2025-04-28 20:24:44 -0500

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,57 +1,145 @@
-/*Comentario Multi-linea*/
-// Comentario de una linea
-
-//KEYWORDS o palabras reservadas
-//var palabra reservada
 /*
-console.log("Hola, Jhon!");
-
-
-
-console.log("1");
-//console.log("2");
-console.log("3");
+Comentario
+Multi-línea
 */
-/*VARIABLES*/
+
+// Comentario de una línea
+
+// KEYWORDS o palabras reservadas
+
+/* VARIABLES */
 /*
-nombreVariable1 = "automática";
+nombreVariable1 = 'automática';
+nombreVariable1 = '1';
 console.log(nombreVariable1);
+
 var nombreVariable2 = 'usando "var"';
+nombreVariable2 = '2';
 console.log(nombreVariable2);
-// durante la ejecucion del programa el valor sea reutilizado
 
-let nombreVariable3 = "usando 'lef'";
+let nombreVariable3 = "usando 'let'";
+nombreVariable3 = '3';
 console.log(nombreVariable3);
-//durante la ejecución solo se utlize una vez
 
-const nombreVariable4 = "usando 'const'";
+const nombreVariable4 = 'usando "const"';
 console.log(nombreVariable4);
 */
 
+// Interpolación de variables
 
- /*OPERADORES*/
- //=
- // ASIGNACION
- 
- 
- /*TIPOS DE DATOS PRMITIVOS*/
- //undefined
- // String, cadena de texto
-/*
- let miVariable;
- miVariable = undefined;
+/* TIPOS DE DATO PRIMITIVOS */
+// undefined
 
+let miVariable;
+miVariable = undefined;
+// string
+miVariable = "'usando comillas dobles'";
+miVariable = 'usando comillas sencillas';
+miVariable = `usando backtips`;
+// number
 miVariable = 0;
-console.log(miVariable)
-
+miVariable = -1;
+miVariable = 1;
+miVariable = 1.5;
+miVariable = 0b01;
+miVariable = 0o01;
+miVariable = 0x01;
+// boolean
 miVariable = true;
 miVariable = false;
-console.log("miVariable:");
+
+/*
+Crear 3 constantes
+- nombre
+- apellido
+- edad
+imprima en la consola su nombre completo y edad usando los valores de las constantes
+Ej de salida: Soy Pepita Pérez y tengo 32 años.
+
+const nombre = 'Pepita';
+const apellido = 'Pérez';
+const edad = 32;
+console.log('Soy Pepita Pérez y tengo 32 años.');
+console.log('Soy', nombre, apellido, 'y tengo', edad, 'años.');
+console.log('Soy ' + nombre + ' ' + apellido + ' y tengo ' + edad + ' años.');
+console.log(`Soy ${nombre} ${apellido} y tengo ${edad} años.`);
+*/
+
+/* OPERADORES */
+// Asignación
+/*
+let
+let miVariable1 = 'Pepita';
+let miVariable2 = 'Pérez';
+
+// Concatenación
+const nombreCompleto = miVariable1 + miVariable2;
+// console.log('nombreCompleto:', nombreCompleto);
+miVariable2 = 5;
+let resultado = miVariable1 + miVariable2;
+
+// Aritméticos
+miVariable1 = 10;
+resultado = miVariable1 + miVariable2;
+resultado = miVariable1 - miVariable2;
+resultado = miVariable1 * miVariable2;
+resultado = miVariable1 / miVariable2;
+resultado = miVariable1 % miVariable2;
+resultado = miVariable1 ** miVariable2;
+
+/*
+- Cree dos variables de tipo constante y asigne los valores 20 y 3 respectivamente
+- Cree una constante para guardar el resultado de cada una de las operaciones básicas (suma, resta, multiplicación y división)
+- Imprima el resultado de cada operación con el siguiente formato de ejemplo:
+20 + 3 = 23
+const num1 = 10;
+const num2 = 5;
+
+const sumResult = num1 + num2;
+const subResult = num1 - num2;
+const mulResult = num1 * num2;
+const divResult = num1 / num2;
+
+const result = `
+${num1} + ${num2} = ${sumResult}
+${num1} - ${num2} = ${subResult}
+${num1} x ${num2} = ${mulResult}
+${num1} / ${num2} = ${divResult}
+`;
+
+console.log(result);
+*/
+
+// Precedencia de operadores Ej: 12 / 3 + 2 * 3 - 1
+// PEMDAS | Paréntesis Exponentes Multiplicación División Adición Sustracción
+/* resultado = 12 / 3 + 2 * 3 - 1;
+console.log('resultado:', resultado); */
+
+//comparasión
+
+/*
+miVariable1 = "Hola";
+miVariable2 = "Hola";
+resultado = miVariable1 == miVariable2;
+console.log("resultado:", resultado);
+resultado = miVariable1 === miVariable2;
+
+console.log("miVariable1:", miVariable1);
+console.log("miVariable2:", miVariable2);
+console.log("resultado:", resultado);
 */
 
 
-const nombre = "Jhon Alejandro";
-const apellido = "Ramirez";
-const edad = 27;;
+//logicos
 
-console.log('Soy ' + nombre + apellido +" y tengo " + edad + " años. ");
+miVariable1 = 10;
+miVariable2 = 5;
+
+resultado = miVariable1 > miVariable2 && miVariable1 > 1;
+
+
+console.log("miVariable1:", miVariable1);
+console.log("miVariable2:", miVariable2);
+console.log("resultado:", resultado);
+
+
