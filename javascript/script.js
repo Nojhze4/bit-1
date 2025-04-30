c96b4c0d6601a7b0e456fa20bbdad26c70bffe42
Jhon Ramirez <jhramirez9716@gmail.com>
2025-04-29 21:51:32 -0500

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,9 @@
 "use strict"
+const $form = document.getElementById("form");
+
+$form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("enviando información del formulario");
+    alert()
+});
+
