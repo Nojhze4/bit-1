1428285b35d87153bd6e816e0f7e50212cf877c1
Jhon Ramirez <jhramirez9716@gmail.com>
2025-05-07 22:17:13 -0500

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,1 @@
-'use strict'
-const $form = document.getElementById("form");
-
-$form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const name = $form.name.value;
-  const message = $form.message.value;
-  const email = $form.email.value;
-  const result = `${name} gracias por tu mensaje: "${message}".  Pronto te responderemos.`;
-  alert(result);
-  $form.reset();
-});
 
